000d6ac1309b6b73df5774a6eb4eb97f490c52f3
Viola Amelia <you@example.com>
2024-06-23 21:37:29 +0700

    update

--- prepo n viz.R
+++ prepo n viz.R
@@ -1,2 +1,21 @@
+#library
+library(tidyverse)
+library(skimr)
+library(ggplot2)
+library(dplyr)
+
+#import data
 data <-  read.csv("Hotel Reservations.csv")
+head(data)
+
+#data preprocessing
 str(data)
+summary(data)
+skim(data)
+
+#pengecekan missing value dan duplicated data
+colSums(is.na(data))
+#no missing value
+duplicated_rows <- duplicated(data)
+dim(data[duplicated_rows, ])
+#no duplicate data
